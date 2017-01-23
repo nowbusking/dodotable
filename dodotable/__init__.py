1021a474e3255731ff57058911821ebf524e4a47
ed <ed@spoqa.com>
2017-01-23 17:23:23 +0900

    0.2.0

--- dodotable/__init__.py
+++ dodotable/__init__.py
@@ -2,5 +2,5 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
-__version_info__ = 0, 1, 3
+__version_info__ = 0, 2, 0
 __version__ = '.'.join(str(v) for v in __version_info__)
