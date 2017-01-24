71634abf6d37fd69988f7b913a73baf9fb35db84
Heejong Ahn <heejongahn@gmail.com>
2017-01-24 15:46:13 +0900

    0.2.1

--- dodotable/__init__.py
+++ dodotable/__init__.py
@@ -2,5 +2,5 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
-__version_info__ = 0, 2, 0
+__version_info__ = 0, 2, 1
 __version__ = '.'.join(str(v) for v in __version_info__)
