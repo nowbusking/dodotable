d1c81a20786b3cc250bdb8dc4b9a643a2bb776f6
Hyojun Kang <admire9@gmail.com>
2017-02-18 01:07:55 +0900

    Remove unused test helper

--- tests/helper.py
+++ tests/helper.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
-import re
-
 from bs4 import BeautifulSoup
-
-try:
-    from urllib import unquote
-except ImportError:
-    from urllib.parse import unquote
 
 from dodotable.environment import Environment
 
@@ -22,32 +15,6 @@ def extract_soup(renderable):
     return BeautifulSoup(renderable.__html__(), 'lxml')
 
 
-def removed_spaces(html):
-    # 태그 시작과 끝에 빈칸을 삭제합니다.
-    html = re.sub(r'(>\s+)', r'>', html)
-    html = re.sub(r'(\s+<)', r'<', html)
-    # attribute값 근처의 빈칸을 삭제합니다.
-    html = re.sub(r'("\s+)', r'" ', html)
-    html = re.sub(r'(\s+")', r' "', html)
-    # 줄바꿈을 삭제합니다.
-    html = re.sub(r'\n', '', html)
-    # 셀프클로징을 없앱니다
-    html = re.sub(r' \/>', '>', html)
-    return html
-
-
-def html_unquote(html):
-    html = unquote(html)
-    replacements = [
-        ('&amp;', '&'),
-        ('&gt;', '>'),
-        ('&lt;', '<'),
-    ]
-    for target, replacement in replacements:
-        html = html.replace(target, replacement)
-    return html
-
-
 class DodotableTestEnvironment(Environment):
 
     def build_url(self, *args, **kwargs):
@@ -56,56 +23,3 @@ class DodotableTestEnvironment(Environment):
 
     def get_session(self):
         return None
-
-
-pager_html = u'''
-<ul class="pager">
-  <li class="page-stepper">Previous</li>
-  <li>
-      <ol class="pager-pages">
-          <li class="first"><span class="selected">1</span></li>
-      </ol>
-  </li>
-  <li class="page-stepper">Next</li>
-</ul>
-'''
-
-table_html = u'''
-<div class="table-wrap">
-
-    <div class="table-header-wrap">
-      <div class="table-header">
-        <h5 class="table-title">
-          {title}
-        </h5>
-
-        <div class="table-filters">
-          {filters}
-        </div>
-
-        <div class="table-information">
-          There is {count} {unit_label} item.
-        </div>
-      </div>
-
-    </div>
-
-
-    <table class="table">
-
-        <thead>
-          {columns}
-        </thead>
-
-        <tbody>
-          {rows}
-        </tbody>
-    </table>
-
-    <div class="table-footer">
-        {pager}
-        <div class="limit-view">
-        </div>
-    </div>
-</div>
-'''.format
