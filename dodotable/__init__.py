049ea9ce9401da155a6726898fc45383cb5bab20
ed <ed@spoqa.com>
2017-02-20 13:31:41 +0900

    Version bump 0.3.9

--- dodotable/__init__.py
+++ dodotable/__init__.py
@@ -2,5 +2,5 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
-__version_info__ = 0, 3, 8
+__version_info__ = 0, 3, 9
 __version__ = '.'.join(str(v) for v in __version_info__)
